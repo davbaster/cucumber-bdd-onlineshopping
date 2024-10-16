ebe91f5717b912a57275a5eab6c9f374fa0a6aba
davbaster <davbaster@gmail.com>
2024-10-16 16:44:29 -0600

    Update StepDefinitions.java removed step def
    Removed the unused step `@Given("I have a {string} in my shopping cart")

--- src/test/java/stepdefinitions/StepDefinitions.java
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -34,25 +34,9 @@ public class StepDefinitions {
         shoppingCart.clear();
     }
 
-    @Given("I have {string} in my shopping cart")
-    public void i_have_item_in_my_shopping_cart(String item) {
-        shoppingCart.clear(); // Clear previous items for simplicity
-        shoppingCart.add(item);
-    }
-
     @When("I add {string} to the cart")
     public void i_add_to_the_cart(String item) {
         shoppingCart.add(item);
-    }
-
-    @When("I add a {string} to the cart")
-    public void i_add_a_to_the_cart(String item) {
-        shoppingCart.add(item);
-    }
-
-    @When("I remove the {string} from the cart")
-    public void i_remove_the_from_the_cart(String item) {
-        shoppingCart.remove(item);
     }
 
     @When("I add {string} to the cart {int} times")
@@ -76,24 +60,11 @@ public class StepDefinitions {
         }
     }
 
-    @Then("the cart should contain {int} item")
-    public void the_cart_should_contain_item(Integer itemCount) {
-        if (shoppingCart.size() != itemCount) {
-            throw new AssertionError("The number of items in the cart is incorrect. Expected: " + itemCount + " but got: " + shoppingCart.size());
-        }
-    }
-
-    @Then("the cart should display {string}")
-    public void the_cart_should_display(String item) {
-        if (!shoppingCart.contains(item)) {
-            throw new AssertionError("The cart does not contain the expected item: " + item);
-        }
-    }
-
-    @Then("the cart should display {string} and {string}")
-    public void the_cart_should_display_and(String firstItem, String secondItem) {
-        if (!(shoppingCart.contains(firstItem) && shoppingCart.contains(secondItem))) {
-            throw new AssertionError("The cart does not contain the expected items: " + firstItem + " and " + secondItem);
+    @Then("the cart should display {string} with quantity {int}")
+    public void the_cart_should_display_with_quantity(String item, Integer quantity) {
+        long count = shoppingCart.stream().filter(i -> i.equals(item)).count();
+        if (count != quantity) {
+            throw new AssertionError("The quantity of the item in the cart is incorrect. Expected: " + quantity + " but got: " + count);
         }
     }
 
@@ -103,21 +74,6 @@ public class StepDefinitions {
         double expectedPriceAsDouble = Double.parseDouble(expectedPrice.replace("$", ""));
         if (Math.abs(totalPrice - expectedPriceAsDouble) > 0.01) {
             throw new AssertionError("Total price is incorrect. Expected: $" + expectedPriceAsDouble + ", but got: $" + totalPrice);
-        }
-    }
-
-    @Then("the cart should display {string} with quantity {int}")
-    public void the_cart_should_display_with_quantity(String item, Integer quantity) {
-        long count = shoppingCart.stream().filter(i -> i.equals(item)).count();
-        if (count != quantity) {
-            throw new AssertionError("The quantity of the item in the cart is incorrect. Expected: " + quantity + " but got: " + count);
-        }
-    }
-
-    @Then("the cart should be empty")
-    public void the_cart_should_be_empty() {
-        if (!shoppingCart.isEmpty()) {
-            throw new AssertionError("The cart is not empty as expected.");
         }
     }
 
